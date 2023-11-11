d0a74c2a7d893cc70af3a6a5b04bf8e9cda5103b
Andrew Plummer <plummer.andrew@gmail.com>
2023-11-12 02:57:00 +0900

    bumped some dev deps

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,7 +1,9 @@
+const { devDependencies } = require('./package.json');
+
 module.exports = {
   mongodbMemoryServerOptions: {
     binary: {
-      version: '6.0.2',
+      version: devDependencies.mongodb,
       skipMD5: true,
     },
     autoStart: false,
